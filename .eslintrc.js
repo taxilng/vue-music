34fc1baadf4abe11acbffa7dc9df0b8cb01aab32
telanks <769967440@qq.com>
2018-12-03 01:46:58 +0800

    轮播图

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,8 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    'semi': 0,
+    "space-before-function-paren":  0,
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
