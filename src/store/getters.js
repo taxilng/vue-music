a8559cfb54ff01fd58405857b2d900928dea2608
taxilng <769967440@qq.com>
2018-12-24 20:57:24 +0800

    vuex播放器相关数据

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,1 +1,8 @@
 export const singer = state => state.singer
+export const playing = state => state.playing
+export const fullScreen = state => state.fullScreen
+export const palylist = state => state.palylist
+export const sequenceList = state => state.sequenceList
+export const mode = state => state.mode
+export const currentIndex = state => state.currentIndex
+export const currentSong = state => state.palylist[state.currentIndex] || {}
